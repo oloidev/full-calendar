1dabf0196113969786650aa66ca06bdff5842884
YassirJr <yassir.junior.03@gmail.com>
2025-04-11 23:12:49 +0100

    remove start/end time

--- src/modules/calendar/schemas.ts
+++ src/modules/calendar/schemas.ts
@@ -6,27 +6,9 @@ export const eventSchema = z.object({
   startDate: z.date({
     required_error: "Start date is required",
   }),
-  startTime: z.object(
-    {
-      hour: z.number(),
-      minute: z.number(),
-    },
-    {
-      required_error: "Start time is required",
-    }
-  ),
   endDate: z.date({
     required_error: "End date is required",
   }),
-  endTime: z.object(
-    {
-      hour: z.number(),
-      minute: z.number(),
-    },
-    {
-      required_error: "End time is required",
-    }
-  ),
   color: z.enum(["blue" , "green" , "red" , "yellow" , "purple" , "orange"], {
     required_error: "Variant is required",
   }),
