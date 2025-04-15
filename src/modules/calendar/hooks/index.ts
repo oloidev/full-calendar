80a15b7abb4bfb3c05000f50afe1db252a03d0ed
YassirJr <yassir.junior.03@gmail.com>
2025-04-15 16:49:29 +0100

    move useFilteredEvents to hooks

--- src/modules/calendar/hooks/index.ts
+++ src/modules/calendar/hooks/index.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {useCalendar} from "@/modules/calendar/contexts/calendar-context";
 
 export function useDisclosure({ defaultIsOpen = false }: { defaultIsOpen?: boolean } = {}) {
   const [isOpen, setIsOpen] = useState(defaultIsOpen);
@@ -10,4 +11,28 @@ export function useDisclosure({ defaultIsOpen = false }: { defaultIsOpen?: boole
   return { onOpen, onClose, isOpen, onToggle };
 }
 
+export const useFilteredEvents = () => {
+  const {  events , selectedDate , selectedUserId} = useCalendar();
 
+  return events.filter((event) => {
+    const itemStartDate = new Date(event.startDate);
+    const itemEndDate = new Date(event.endDate);
+
+    const monthStart = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+        1
+    );
+    const monthEnd = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth() + 1,
+        0
+    );
+
+    const isInSelectedMonth =
+        itemStartDate <= monthEnd && itemEndDate >= monthStart;
+    const isUserMatch =
+        selectedUserId === "all" || event.user.id === selectedUserId;
+    return isInSelectedMonth && isUserMatch;
+  });
+}
