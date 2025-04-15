fbeba6fa613b03787382eb25e5a76d477e7ef8be
YassirJr <yassir.junior.03@gmail.com>
2025-04-15 16:44:26 +0100

    add support for dot/colored view

--- src/modules/calendar/components/agenda-view/agenda-events.tsx
+++ src/modules/calendar/components/agenda-view/agenda-events.tsx
@@ -8,62 +8,83 @@ import {
     CommandItem,
     CommandList,
 } from "@/components/ui/command";
-import {format, getTime} from "date-fns";
-import {Repeat} from "lucide-react";
+import {format, parseISO} from "date-fns";
 import {cn} from "@/lib/utils";
-import {formatTime, getBgColor, getColorClass, useGetEventsByMode} from "@/modules/calendar/helpers";
+import {formatTime, getBgColor, getColorClass, getFirstLetters, useGetEventsByMode} from "@/modules/calendar/helpers";
 import {EventDetailsDialog} from "@/modules/calendar/components/dialogs/event-details-dialog";
-import {getFirstLetters} from "@/modules/calendar/helpers";
 import {useCalendar} from "@/modules/calendar/contexts/calendar-context";
+import {EventBullet} from "@/modules/calendar/components/month-view/event-bullet";
 
 export const AgendaEvents: FC = () => {
-    const {events , use24HourFormat} = useCalendar();
+    const {events, use24HourFormat, badgeVariant} = useCalendar();
 
     const eventsByMode = Object.groupBy(useGetEventsByMode(events), (event) => {
-        return format(event.startDate, 'yyyy-MM-dd');
+        return format(parseISO(event.startDate), 'yyyy-MM-dd');
     });
 
     const groupedAndSortedEvents = Object.entries(eventsByMode).sort((a, b) =>
-        getTime(a[0]) - getTime(b[0])
+        new Date(a[0]).getTime() - new Date(b[0]).getTime()
     );
 
     return (
         <Command className="py-4 h-[80vh] bg-transparent">
-            <div className="mb-4 mx-4 shadow-md">
+            <div className="mb-4 mx-4">
                 <CommandInput placeholder="Type a command or search..."/>
             </div>
             <CommandList className="max-h-max px-3">
                 {groupedAndSortedEvents.map(([date, groupedEvents]) => (
-                    <CommandGroup key={date} heading={format(date, "MMMM d, yyyy")}>
+                    <CommandGroup key={date} heading={format(new Date(date), "EEEE, dd MMMM yyyy")}>
                         {groupedEvents!.map((event) => (
                             <CommandItem
                                 key={event.id}
                                 className={cn(
-                                    "mb-2 p-4 border rounded-md data-[selected=true]:bg-bg data-[selected=true]:text-none hover:cursor-pointer",
-                                    getColorClass(event.color)
+                                    "mb-2 p-4 border rounded-md data-[selected=true]:bg-bg hover:bg-zinc-200 dark:hover:bg-gray-900 transition-all data-[selected=true]:text-none hover:cursor-pointer",
+                                    {
+                                        [getColorClass(event.color)]: badgeVariant === "colored",
+                                    }
                                 )}>
                                 <EventDetailsDialog event={event}>
                                     <div className="w-full flex items-center justify-between gap-4">
                                         <div className="flex items-center gap-2">
-                                            <Avatar>
-                                                <AvatarImage src="" alt="@shadcn"/>
-                                                <AvatarFallback className={getBgColor(event.color)}>
-                                                    {getFirstLetters(event.title)}
-                                                </AvatarFallback>
-                                            </Avatar>
-                                            <p>{event.title}</p>
+                                            {badgeVariant === "dot" ? (
+                                                <EventBullet color={event.color}  />
+                                            ) : (
+                                                <Avatar>
+                                                    <AvatarImage src="" alt="@shadcn"/>
+                                                    <AvatarFallback className={getBgColor(event.color)}>
+                                                        {getFirstLetters(event.title)}
+                                                    </AvatarFallback>
+                                                </Avatar>
+                                            )}
+                                            <p className={cn({
+                                                "font-medium": badgeVariant === "dot",
+                                                "text-foreground": badgeVariant === "dot"
+                                            })}>{event.title}</p>
                                         </div>
-                                        <div className="w-40 sm:w-auto flex items-center gap-4">
+                                        <div className="w-40 sm:w-auto flex items-center gap-1">
                                             <p className="flex flex-wrap">
-                                            <span className="block">
-                                                {formatTime(event.startDate, use24HourFormat)}
-                                            </span>
+                                                <span className={cn("block", {
+                                                    "text-muted-foreground": badgeVariant === "dot",
+                                                    "text-muted": badgeVariant === "colored"
+                                                })}>
+                                                    {formatTime(event.startDate, use24HourFormat)}
+                                                </span>
                                             </p>
-                                            <Repeat/>
-                                            <p className="flex flex-wrap">
-                                            <span className="block">
-                                                {formatTime(event.endDate, use24HourFormat)}
+                                            <span
+                                                className={cn({
+                                                    "text-muted-foreground": badgeVariant === "dot",
+                                                    "text-muted": badgeVariant === "colored"
+                                                })}
+                                            >
+                                             -
                                             </span>
+                                            <p className="flex flex-wrap">
+                                                <span className={cn("block", {
+                                                    "text-muted-foreground": badgeVariant === "dot",
+                                                    "text-muted": badgeVariant === "colored"
+                                                })}>
+                                                    {formatTime(event.endDate, use24HourFormat)}
+                                                </span>
                                             </p>
                                         </div>
                                     </div>
