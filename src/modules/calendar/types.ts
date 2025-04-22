9e80f101aa7517a0b90388e2adeb77c0c8fea1e6
YassirJr <yassir.junior.03@gmail.com>
2025-04-22 21:03:41 +0100

    add agenda to TCalendarViewc type

--- src/modules/calendar/types.ts
+++ src/modules/calendar/types.ts
@@ -1,2 +1,2 @@
-export type TCalendarView = "day" | "week" | "month" | "year";
+export type TCalendarView = "day" | "week" | "month" | "year" | "agenda";
 export type TEventColor = "blue" | "green" | "red" | "yellow" | "purple" | "orange";
